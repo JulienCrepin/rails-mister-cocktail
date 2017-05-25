783e58bdd58efcd15838858e5b695fbb2a756101
Julien Crepin <jppcrepin@gmail.com>
2017-05-25 15:04:50 +0200

    Make the mister_cocktail

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+  resources :cocktails, only: [:index, :show, :new, :create] do
+    resources :doses, only: [:new,:create]
+  end
+  resources :doses, only: [:destroy]
+  # get 'doses/index' to: 'doses#index'
+
+  # get 'doses/show' to: 'doses#show'
+
+  # get 'doses/new' to: 'doses#new'
+
+  # post 'doses/create' to: 'doses#create'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
